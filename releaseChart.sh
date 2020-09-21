d0214f0e86bab72f7a7cf871511c8e31f959828b
Dennis Ploeger <dennis.ploeger@kps.com>
2020-09-21 14:29:27 +0200

    feat: Removed redundant parameter

--- releaseChart.sh
+++ releaseChart.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-then
-  echo "Usage: releaseChart.sh <version>"
-  exit 1
-fi
-
-VERSION="$1"
-
 if [ "$(git branch --show-current)" != "gh-pages" ]
 then
   echo "Merge to gh-pages first!"
